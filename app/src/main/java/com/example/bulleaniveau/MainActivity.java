1c1cca0be5a127b6f7278a44c280cc0bef9dbe1e
Simeon-B <simeon.brulisauer@divtec.ch>
2023-05-16 10:45:58 +0200

    Affiche des axes selon x, y et z

--- app/src/main/java/com/example/bulleaniveau/MainActivity.java
+++ app/src/main/java/com/example/bulleaniveau/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bulleaniveau;
 
+// importation des outils
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -9,17 +10,31 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+/**
+ * Mon application
+ * qui test les capteurs de GRAVITY
+ */
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     // initialisation des variable
     private SensorManager sensorManager;
     private Sensor gravitySensor;
     private TextView orientationTextView;
-    private View lineView;
+    private View lineViewx;
+    private View lineViewy;
+    private View lineViewz;
 
-
+    /**
+     * début de l'application s'éxecute uniquement lors du démarage de l'application
+     * org.codehaus.groovy.control.MultipleCompilationErrorsException: startup failed:
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +42,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         // récupère le textView
         orientationTextView = findViewById(R.id.orientationTextView);
+        lineViewx = findViewById(R.id.lineViewx);
+        lineViewy = findViewById(R.id.lineViewy);
+        lineViewz = findViewById(R.id.lineViewz);
+
 
         // récupère le sensor en mode gravity
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -48,16 +67,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
+    /**
+     * Regarde si les capteurs change et change en même temps les affichages
+     *
+     * @param event the {@link android.hardware.SensorEvent SensorEvent}.
+     */
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        // Mise à jour des valeurs de x et y
+        // Mise à jour des valeurs de x, y et z
         int xValue = (int) (event.values[0] * 10);
-        int yValue = (int) (event.values[1] * 10);
+        int yValue = (int) (event.values[1] * -10);
         int zValue = (int) (event.values[2] * 10);
 
         // Mise à jour de l'affichage dans le TextView
         orientationTextView.setText("x : " + xValue + "%\ny : " + yValue + "%\nz : " + zValue + "%");
+
+        // Mise à jour de l'affichage des éguilles des axes
+        lineViewx.setRotation(xValue);
+        lineViewy.setRotation(yValue);
+        ViewGroup.LayoutParams layoutParamsx = lineViewz.getLayoutParams();
+        ViewGroup.LayoutParams layoutParamsz = lineViewz.getLayoutParams();
+        layoutParamsx.height = (int) ((xValue < 0 ? xValue * -1 : xValue) * 3.5);
+        layoutParamsz.height = (int) ((zValue < 0 ? zValue * -1 : zValue) * 3.5);
+        lineViewz.setLayoutParams(layoutParamsz);
+        lineViewx.setLayoutParams(layoutParamsx);
     }
 
     @Override
